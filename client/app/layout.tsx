bbe47e487963f0da5c8032b1e929eaa4eed1c9e6
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-06-09 01:32:34 +0530

    extra lines of code removed

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/context/themeProvider";
-// import { getServerSession } from "next-auth";
-// import SessionProvider  from "@/context/sessionProvider";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,21 +10,16 @@ export const metadata: Metadata = {
   description: "Fullstack Chatting App",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  // const session = await getServerSession();
-
   return (
-      <ThemeProvider>
-        {/* <SessionProvider session = {session}> */}
-          <html lang="en">
-            <body className={inter.className}>{children}</body>
-          </html>
-        {/* </SessionProvider> */}
-      </ThemeProvider>
+    <ThemeProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ThemeProvider>
   );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,26 +1,10 @@
-'use client'
+"use client";
 import HomePage from "@/components/HomePage";
 import Navbar from "@/components/shared/Navbar";
 import useTheme from "@/hooks/useTheme";
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "./api/auth/[...nextauth]";
-// import { redirect } from "next/navigation";
-// import { useRouter } from "next/router";
 
 export default function Home() {
   const { theme } = useTheme();
-  // const session = await getServerSession (authOptions);
-  // const route = useRouter();
-
-  // if (!session){
-  //   redirect("./authPage/login");  
-    // return(
-    // )
-  // }
-
-  // const push =()=>{
-  //   route.push("./auth/login")
-  // }
 
   return (
     <div className={`${theme} h-screen`}>
