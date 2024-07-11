f95c9b17f0879af6831994efd05bc6639bbfdfa0
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-07-12 02:30:12 +0530

    userdashboard ui updated

--- client/components/shared/UserDashBoard.tsx
+++ client/components/shared/UserDashBoard.tsx
@@ -1,6 +1,6 @@
 import useUserChat from "@/hooks/useUserChat";
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { MotionConfig, motion } from "framer-motion";
 import Image from "next/image";
 import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
@@ -9,10 +9,25 @@ import DuoRoundedIcon from '@mui/icons-material/DuoRounded';
 
 const UserDashBoard = () => {
   const { setShowUserDetails, showUserDetails, userName, userImage } = useUserChat();
+
+  useEffect(()=>{
+    const handleKeyDown = (event:any) => {
+      if (event.key === 'Escape') {
+        closeUserDashboard();
+      }
+    };
+    if (showUserDetails) {
+      document.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  },[showUserDetails]);
+
   const closeUserDashboard = () => {
     setShowUserDetails(!showUserDetails);
   };
-
   return (
     <MotionConfig transition={{ duration: 1.5 }}>
       <motion.div className="h-[80vh]" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
@@ -23,6 +38,7 @@ const UserDashBoard = () => {
         <div className="h-full w-full flex flex-col justify-center items-center">
           <Image src={userImage} alt="@shadcn" className="rounded-full" width={200} height={200} />
           <h1 className="m-5 text-[25px]">{userName}</h1>
+          <p className="my-4 w-[35vw]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laborum unde nemo cumque molestias illum molestiae tempora cum quam maxime!</p>
           <div className="flex justify-between w-[20vw] border-t border-muted p-4">
               <Button className="rounded-lg shadow-sm shadow-green-600 px-8 py-4" onClick={closeUserDashboard}><ChatRoundedIcon className="text-[30px] "/></Button>
               <Button className="rounded-lg shadow-sm shadow-green-600 px-8 py-4"><CallRoundedIcon className="text-[30px] "/></Button>
