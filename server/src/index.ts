da743e344ade0a3e27c622137e852669a56dbde2
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-07-17 01:23:15 +0530

    socket server fixed

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,6 @@ import { SocketService } from './services/socket';
 import express from 'express';
 import cors from 'cors'
 
-
 const app = express();
 const port = process.env.PORT || 4000;
 
--- server/src/services/socket.ts
+++ server/src/services/socket.ts
@@ -1,5 +1,11 @@
 import { Server } from "socket.io"
-import prisma from "../db";
+import { createAdapter } from "@socket.io/mongo-adapter";
+import { MongoClient } from "mongodb";
+
+const DB = "messages";
+const COLLECTION = "socket.io-adapter-events";
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL as string);
 
 export class SocketService {
     private _io: Server;
@@ -14,29 +20,41 @@ export class SocketService {
     }
 
     // we can send message to the particular user using this userID
-    public sendMessage() {
+    public async sendMessage() {
         const io = this.io;
+        await mongoClient.connect();
+
+        try {
+            await mongoClient.db(DB).createCollection(COLLECTION, {
+                capped: true,
+                size: 1e6
+            });
+        } catch (e) {
+            console.log("mongo adapter error", e);            
+        }
+
+        const mongoCollection = mongoClient.db(DB).collection(COLLECTION);
+        io.adapter(createAdapter(mongoCollection));
+
+
         io.on("connection", (socket) => {
             // here i have to save the message into the db for that particular user.
-           
+
             socket.on('join', (userId: string) => {
                 // Validate the userId and fetch the user from MongoDB
                 socket.join(userId);
                 console.log(`User ${userId} joined with socket id ${socket.id}`);
             });
 
+
             socket.on('send-message', (reciever) => {
-                const {receipentId,message} = reciever;
+                const { receipentId, message } = reciever;
                 // Validate the userId and fetch the user from MongoDB                
-                if (receipentId){
-                    io.to(receipentId).emit('receive-message',{
-                        senderId:socket.id,
-                        message:message,
-                        status: 'recieved',
-                    })
+                if (receipentId) {
+                    io.to(receipentId).emit('receive-message',message);
                     console.log(`Reciepent ${receipentId} is online and the message is ${message}`);
                 }
-                else{
+                else {
                     console.log(`Reciepent ${receipentId} is offline`);
                 }
             });
