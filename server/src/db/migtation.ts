a7302dfe0b693f059d8d74b0664e348fa7f77f2c
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-07-19 13:20:02 +0530

    migration file added

---
+++ server/src/db/migtation.ts
@@ -0,0 +1,11 @@
+import prisma from ".";
+
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log('All migrations completed successfully');
+  } catch (error) {
+    console.error('Error running migrations:', error);
+  }
+}
